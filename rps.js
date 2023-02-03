ec61b8b2b1a24942e1fd0f1be06bf162e10d0f46
Kyle <xkylechan@gmail.com>
2023-02-03 12:15:18 -0700

    Got external files to link and work with game

--- rps.js
+++ rps.js
@@ -1,216 +1,124 @@
-let win = 0;
-let lose = 0;
-let tie = 0;  
 
-function getComputerChoice() {
-  let pick = Math.random();
-  if (pick >= 0.66) {
-    return 'Rock';
-  }
-  else if (pick <= 0.33) {
-    return 'Paper';
-  }
-  else {
-    return 'Scissors';
-  }
-}
+      let win = 0;
+      let lose = 0;
+      let tie = 0;  
 
-function playRound(playerSelection) {
-  let computerSelection = getComputerChoice();
+      function getComputerChoice() {
+        let pick = Math.random();
+        if (pick >= 0.66) {
+          return 'Rock';
+        }
+        else if (pick <= 0.33) {
+          return 'Paper';
+        }
+        else {
+          return 'Scissors';
+        }
+      }
 
-  computerPick.textContent = 'Computer Picked: ' + computerSelection;
-  
-  playerPick.textContent = 'Player Picked: ' + playerSelection;
+      function playRound(playerSelection) {
+        let computerSelection = getComputerChoice();
 
-
-
-  displayPicks.appendChild(computerPick);
-  displayPicks.appendChild(playerPick);
+        computerPick.textContent = 'Computer Picked: ' + computerSelection;
+        
+        playerPick.textContent = 'Player Picked: ' + playerSelection;
 
 
 
-    if (win < 5 && lose < 5) { 
-      if (playerSelection === computerSelection) {
-      tie = tie + 1
-      winner.textContent = "It's a TIE!";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-
-    else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-      lose = lose + 1
-      winner.textContent = "You LOSE! Rock loses to Paper";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-
-    }
-    else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-      win = win + 1
-      winner.textContent = "You WIN! Rock beats Scissors";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-    else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-      win = win + 1
-      winner.textContent = "You WIN! Paper beats Rock";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-    else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-      lose = lose + 1
-      winner.textContent = "You LOSE! Paper loses to Scissors";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-
-    }
-    else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-      win = win + 1
-      winner.textContent = "You WIN! Scissors beats Paper";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-    else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-      lose = lose + 1
-      winner.textContent = "You LOSE! Scissors loses to Rock";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-  }
-  else {
-    gameWinner();
-  }
-}
-
-function gameWinner() {
-  if (win > lose) {
-    alert(`You WIN! The score: Player - ${win} // Computer - ${lose}`);
-  }
-  else if (lose > win) {
-    alert(`You LOSE! The score: Player - ${win} // Computer - ${lose}`);
-  }
-}
-
-const rockBtn = document.querySelector('#rockBtn');
-rockBtn.addEventListener('click', () =>  {
-  playerSelection = 'Rock'; 
-  console.log(playRound(playerSelection));
-}); 
-
-const paperBtn = document.querySelector('#paperBtn');
-paperBtn.addEventListener('click', () =>  {
-  playerSelection = 'Paper'; 
-  console.log(playRound(playerSelection));
-}); 
-
-const scissorsBtn = document.querySelector('#scissorsBtn');
-scissorsBtn.addEventListener('click', () =>  {
-  playerSelection = 'Scissors'; 
-  console.log(playRound(playerSelection));
-}); 
-
-const displayPicks = document.querySelector('#results')
-const score = document.createEleme<script>
-let win = 0;
-let lose = 0;
-let tie = 0;  
-
-function getComputerChoice() {
-  let pick = Math.random();
-  if (pick >= 0.66) {
-    return 'Rock';
-  }
-  else if (pick <= 0.33) {
-    return 'Paper';
-  }
-  else {
-    return 'Scissors';
-  }
-}
-
-function playRound(playerSelection) {
-  let computerSelection = getComputerChoice();
-
-  computerPick.textContent = 'Computer Picked: ' + computerSelection;
-  
-  playerPick.textContent = 'Player Picked: ' + playerSelection;
+        displayPicks.appendChild(computerPick);
+        displayPicks.appendChild(playerPick);
 
 
 
-  displayPicks.appendChild(computerPick);
-  displayPicks.appendChild(playerPick);
+          if (win < 5 && lose < 5) { 
+           if (playerSelection === computerSelection) {
+            tie = tie + 1
+            winner.textContent = "It's a TIE!";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
+          }
 
+          else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+            lose = lose + 1
+            winner.textContent = "You LOSE! Rock loses to Paper";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
 
+          }
+          else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+            win = win + 1
+            winner.textContent = "You WIN! Rock beats Scissors";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
+          }
+          else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+            win = win + 1
+            winner.textContent = "You WIN! Paper beats Rock";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
+          }
+          else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+            lose = lose + 1
+            winner.textContent = "You LOSE! Paper loses to Scissors";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
 
-    if (win < 5 && lose < 5) { 
-      if (playerSelection === computerSelection) {
-      tie = tie + 1
-      winner.textContent = "It's a TIE!";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
+          }
+          else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+            win = win + 1
+            winner.textContent = "You WIN! Scissors beats Paper";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
+          }
+          else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+            lose = lose + 1
+            winner.textContent = "You LOSE! Scissors loses to Rock";
+            displayPicks.appendChild(winner);
+            score.textContent = `Player - ${win} // Computer - ${lose}`
+            displayPicks.appendChild(score);
+          }
+        }
+        else {
+          gameWinner();
+        }
+      }
 
-    else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-      lose = lose + 1
-      winner.textContent = "You LOSE! Rock loses to Paper";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
+      function gameWinner() {
+        if (win > lose) {
+          alert(`You WIN! The score: Player - ${win} // Computer - ${lose}`);
+        }
+        else if (lose > win) {
+          alert(`You LOSE! The score: Player - ${win} // Computer - ${lose}`);
+        }
+      }
 
-    }
-    else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-      win = win + 1
-      winner.textContent = "You WIN! Rock beats Scissors";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-    else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-      win = win + 1
-      winner.textContent = "You WIN! Paper beats Rock";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-    else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-      lose = lose + 1
-      winner.textContent = "You LOSE! Paper loses to Scissors";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
+      const rockBtn = document.querySelector('#rockBtn');
+      rockBtn.addEventListener('click', () =>  {
+        playerSelection = 'Rock'; 
+        console.log(playRound(playerSelection));
+      }); 
 
-    }
-    else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-      win = win + 1
-      winner.textContent = "You WIN! Scissors beats Paper";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-    else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-      lose = lose + 1
-      winner.textContent = "You LOSE! Scissors loses to Rock";
-      displayPicks.appendChild(winner);
-      score.textContent = `Player - ${win} // Computer - ${lose}`
-      displayPicks.appendChild(score);
-    }
-  }
-  else {
-    gameWinner();
-  }
-}
+      const paperBtn = document.querySelector('#paperBtn');
+      paperBtn.addEventListener('click', () =>  {
+        playerSelection = 'Paper'; 
+        console.log(playRound(playerSelection));
+      }); 
 
-function gameWinner() {
-  if (win > lose) {
-    alert(`You WIN! The score: Player - ${win} // Computer - ${lose}`);
-  }
-  else if (lose > win) {
-    alert(`You LOSE! The score: Player - ${win} // Computer - ${lose}`);
-  }
+      const scissorsBtn = document.querySelector('#scissorsBtn');
+      scissorsBtn.addEventListener('click', () =>  {
+        playerSelection = 'Scissors'; 
+        console.log(playRound(playerSelection));
+      }); 
+      
+      const displayPicks = document.querySelector('#results')
+      const score = document.createElement('div');
+      const computerPick = document.createElement('div');
+      const winner = document.createElement('div');
+      const playerPick = document.createElement('div');
+      const winnerOfGame = document.createElement('div');
