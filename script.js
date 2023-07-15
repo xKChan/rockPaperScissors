d8283c7130bfc453f8029a659f5eb3d8035abd7b
kc <xkylechan@gmail.com>
2023-07-14 20:37:13 -0600

    Create a one round game that plays one round and determines the winner

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const getComputerChoice = () => {
     let x = Math.random();
     if (x < 0.33) {
-        console.log("rock");
         return "Rock";
     } else if (x > 0.66) {
-        console.log("Paper");
         return "Paper";
     } else {
-        console.log("Scissors");
         return "Scissors";
     }
 }
 
+const playRound = (playerSelection, computerSelection) => {
+    console.log(`Player: ${playerSelection} // Computer: ${computerSelection}`);
+    if (playerSelection == computerSelection) {
+        return `It's a tie.`;
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
+                (playerSelection == "Paper" && computerSelection == "Rock") || 
+                (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        return `Player wins!`;
+    } else {
+        return `Computer wins!`;
+    }
+}
+
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+console.log(playRound(playerSelection, computerSelection));
