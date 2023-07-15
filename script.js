7feeb2326da8c09ff4c84b8b0d16ed99d123eaa8
kc <xkylechan@gmail.com>
2023-07-14 22:56:40 -0600

    Create a 5 round game, that adds up the score and returns the winner

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const prompt = require('prompt-sync')();
 
+const NUM_GAME = 5;
+
+let tie = 0;
+let win = 0;
+let lose = 0;
+
 function getPlayerChoice() {
     while(true) {
         let playerPick = prompt("Rock, Paper, or Scissors? ");
@@ -24,18 +30,52 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     console.log(`Player: ${playerSelection} // Computer: ${computerSelection}`);
     if (playerSelection == computerSelection) {
-        return `It's a tie.`;
+        console.log(`It's a tie.`);
+        return "tie";
     } else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
         (playerSelection == "Paper" && computerSelection == "Rock") ||
         (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return "win";
     } else {
-        return `You Lose! ${playerSelection} loses to ${computerSelection}`;
+        console.log(`You Lose! ${playerSelection} loses to ${computerSelection}`);
+        return "lose";
     }
 }
 
+function totalScore(score) {
+    if (score == "win") {
+         win++
+    } else if (score == "lose") {
+         lose++
+    } else {
+         tie++
+    }
+    console.log(`Player Score: ${win} || Computer Score: ${lose} || Ties: ${tie}`);
+}
+
+function gameWinner() {
+    if (win > lose && win >= tie) {
+        console.log("Player Wins!");
+        return;
+    } else if (lose > win && lose >= tie) {
+        console.log("Computer Wins!");
+        return;
+    }
+    console.log("It's a tie! No one wins.");
+    return;
+}
+
+function game() {
+    for (let i = 0; i < NUM_GAME; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+
+        let score = playRound(playerSelection, computerSelection);
+        totalScore(score);
+    }
+    gameWinner();
+}
 
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
