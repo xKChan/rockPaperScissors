ab424d818644d8b84f4447ee6b20979752a0584f
kc <xkylechan@gmail.com>
2023-07-14 22:25:55 -0600

    Add a prompt for player to pick what they want, added a node.js to test

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const getComputerChoice = () => {
+const prompt = require('prompt-sync')();
+
+function getPlayerChoice() {
+    while(true) {
+        let playerPick = prompt("Rock, Paper, or Scissors? ");
+        let playerPickCheck = playerPick.charAt(0).toUpperCase() + playerPick.slice(1).toLowerCase();
+        if (playerPickCheck == "Rock" || playerPickCheck == "Paper" || playerPickCheck == "Scissors") {
+            return playerPickCheck;
+        }
+    }
+}
+
+function getComputerChoice() {
     let x = Math.random();
     if (x < 0.33) {
         return "Rock";
@@ -9,21 +21,21 @@ const getComputerChoice = () => {
     }
 }
 
-const playRound = (playerSelection, computerSelection) => {
+function playRound(playerSelection, computerSelection) {
     console.log(`Player: ${playerSelection} // Computer: ${computerSelection}`);
     if (playerSelection == computerSelection) {
         return `It's a tie.`;
-    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
-                (playerSelection == "Paper" && computerSelection == "Rock") || 
-                (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        return `Player wins!`;
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `Computer wins!`;
+        return `You Lose! ${playerSelection} loses to ${computerSelection}`;
     }
 }
 
 
 
 const computerSelection = getComputerChoice();
-const playerSelection = "Rock";
+const playerSelection = getPlayerChoice();
 console.log(playRound(playerSelection, computerSelection));
